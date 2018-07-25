2073774272a61f75f07df0861ed9f8564e1517de
Stephen Hitchner <git@stephenhitchner.com>
2018-07-24 22:47:31 -0700

    working on things

---
+++ generators/sql.go
@@ -0,0 +1,20 @@
+package generators
+
+import (
+	"github.com/graph-gophers/graphql-go/introspection"
+)
+
+type SQLTemplate struct {
+	Imports []string
+}
+
+type SQLResolver struct {
+}
+
+func NewSQLResolver() *SQLResolver {
+
+}
+
+func (t *SQLResolver) Generate(schema *introspection.Schema) error {
+
+}
