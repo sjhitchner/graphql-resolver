bfbfc9a7892df5d3a61900f885967bd21cd7d911
Stephen Hitchner <git@stephenhitchner.com>
2018-09-03 18:24:12 -0700

    working types/domain generation

--- internal/domain/types.go
+++ internal/domain/types.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/stoewer/go-strcase"
 
@@ -28,6 +29,51 @@ type Field struct {
 }
 
 type Method struct {
+	Name   string
+	Args   []Arg
+	Return string
+}
+
+type Arg struct {
+	Name string
+	Type string
+}
+
+type Index struct {
+	Name   string
+	Fields []Field
+}
+
+func (t Index) MethodName() string {
+	return func() string {
+		names := make([]string, 0, len(t.Fields))
+		for _, f := range t.Fields {
+			names = append(names, f.Internal)
+		}
+		return strings.Join(names, "")
+	}()
+}
+
+func (t Model) Indexes() []Index {
+	indexes := make(map[string][]Field)
+	for _, field := range t.Fields {
+		for _, index := range field.Indexes {
+			_, found := indexes[index]
+			if !found {
+				indexes[index] = make([]Field, 0, 10)
+			}
+			indexes[index] = append(indexes[index], field)
+		}
+	}
+
+	i := make([]Index, 0, len(indexes))
+	for name, fields := range indexes {
+		i = append(i, Index{
+			Name:   name,
+			Fields: fields,
+		})
+	}
+	return i
 }
 
 func GenerateModels(config *config.Config) []Model {
@@ -35,17 +81,9 @@ func GenerateModels(config *config.Config) []Model {
 	models := make([]Model, 0, len(config.Models))
 	for _, m := range config.Models {
 
-		/*
-			methods := make([]Method, 0, len(m.Fields))
-			for _, d := range m.Fields {
-				methods = append(methods, Method{
-
-				})
-			}
-		*/
-
 		fields := make([]Field, 0, len(m.Fields))
 		for _, f := range m.Fields {
+
 			if f.Type == "manytomany" {
 				models = append(models, Model{
 					Name:        CamelCasef("%s_%s", m.Name, f.Name),
@@ -82,6 +120,7 @@ func GenerateModels(config *config.Config) []Model {
 				Description: f.Description,
 				Deprecated:  f.Deprecated,
 				Type:        config.TypeMapping(f.Type),
+				Indexes:     f.Indexes,
 			})
 		}
 
@@ -96,8 +135,55 @@ func GenerateModels(config *config.Config) []Model {
 			Description: m.Description,
 			Deprecated:  m.Deprecated,
 			Fields:      fields,
-			//Methods:     methods,
 		})
+	}
+
+	return PopulateMethods(models)
+}
+
+func PopulateMethods(models []Model) []Model {
+
+	for i, m := range models {
+		methods := make([]Method, 0, len(m.Fields))
+		for _, index := range m.Indexes() {
+			verb := "List"
+			if index.Name == "primary" || strings.Contains(index.Name, "unique") {
+				verb = "Get"
+			}
+
+			args := make([]Arg, 0, len(m.Fields))
+			for _, f := range index.Fields {
+				args = append(args, Arg{
+					Name: LowerCamelCasef(f.Internal),
+					Type: f.Type,
+				})
+			}
+
+			methods = append(methods, Method{
+				Name: CamelCasef(
+					"%s_%s%s_by_%s",
+					verb,
+					m.Name,
+					func() string {
+						if verb == "List" {
+							return "s"
+						}
+						return ""
+					}(),
+					index.MethodName(),
+				),
+				Args: args,
+				Return: func() string {
+					prefix := "*"
+					if verb == "List" {
+						prefix = "[]*"
+					}
+					return prefix + CamelCasef("%s", m.Name)
+				}(),
+			})
+		}
+
+		models[i].Methods = methods
 	}
 
 	return models
@@ -105,4 +191,8 @@ func GenerateModels(config *config.Config) []Model {
 
 func CamelCasef(f string, args ...interface{}) string {
 	return strcase.UpperCamelCase(fmt.Sprintf(f, args...))
+}
+
+func LowerCamelCasef(f string, args ...interface{}) string {
+	return strcase.LowerCamelCase(fmt.Sprintf(f, args...))
 }
